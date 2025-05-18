4fd6b69a364f961275faa7447544493250c90cd5
HeeWon <jhwon317@naver.com>
2025-05-19 03:33:25 +0900

    setting

---
+++ src/main/java/com/heeone/basic01/Basic01Application.java
@@ -0,0 +1,14 @@
+package com.heeone.basic01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basic01Application {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Basic01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/heeone/basic01/Basic01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeone.basic01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basic01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
